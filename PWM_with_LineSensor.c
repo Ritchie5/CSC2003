f4bb6602629b751c1a8462a7c7ea6583f21d1507
IvanChewSQ <2001303@sit.singaporetech.edu.sg>
2021-11-09 22:14:25 +0800

    with sensor code, using delay to change direction

--- PWM_with_LineSensor.c
+++ PWM_with_LineSensor.c
@@ -72,33 +72,26 @@ int main(void)
     Timer_A_generatePWM(TIMER_A0_BASE, &right_wheel);
     Timer_A_generatePWM(TIMER_A0_BASE, &left_wheel);
 
-    /* Line sensor: Configuring Output Light.*/
-    GPIO_setAsOutputPin(GPIO_PORT_P1, GPIO_PIN0);
-    GPIO_setAsOutputPin(GPIO_PORT_P2, GPIO_PIN1);
-    GPIO_setOutputLowOnPin(GPIO_PORT_P1, GPIO_PIN0);
-    GPIO_setOutputLowOnPin(GPIO_PORT_P2, GPIO_PIN1);;
-
     /* Configuring P3.7 as Input. Line sensor (right)*/
     GPIO_setAsInputPinWithPullUpResistor(GPIO_PORT_P3, GPIO_PIN7);
-    GPIO_interruptEdgeSelect(GPIO_PORT_P3, GPIO_PIN7, GPIO_HIGH_TO_LOW_TRANSITION);
+    GPIO_interruptEdgeSelect(GPIO_PORT_P3, GPIO_PIN7, GPIO_LOW_TO_HIGH_TRANSITION);
     GPIO_clearInterruptFlag(GPIO_PORT_P3, GPIO_PIN7);
     GPIO_enableInterrupt(GPIO_PORT_P3, GPIO_PIN7);
 
     /* Configuring P5.5 as Input. Line sensor (left)*/
     GPIO_setAsInputPinWithPullUpResistor(GPIO_PORT_P5, GPIO_PIN5);
-    GPIO_interruptEdgeSelect(GPIO_PORT_P5, GPIO_PIN5, GPIO_HIGH_TO_LOW_TRANSITION);
+    GPIO_interruptEdgeSelect(GPIO_PORT_P5, GPIO_PIN5, GPIO_LOW_TO_HIGH_TRANSITION);
     GPIO_clearInterruptFlag(GPIO_PORT_P5, GPIO_PIN5);
     GPIO_enableInterrupt(GPIO_PORT_P5, GPIO_PIN5);
 
     /* Enabling interrupts */
     Interrupt_enableInterrupt(INT_PORT3);
     Interrupt_enableInterrupt(INT_PORT5);
-    Interrupt_enableSleepOnIsrExit();
     Interrupt_enableMaster();
 
     /* Sleeping when not in use */
     while (1){
-        PCM_gotoLPM0();
+        PCM_gotoLPM3InterruptSafe();
     }
 }
 
@@ -109,7 +102,7 @@ void PORT3_IRQHandler(void)
     /*When Line Sensor (right) detects change from light to dark*/
     if (status & GPIO_PIN7){
         /*When Line Sensor detects dark*/
-        if(GPIO_getInputPinValue(GPIO_PORT_P3, GPIO_PIN7) == 0){
+        if(GPIO_getInputPinValue(GPIO_PORT_P3, GPIO_PIN7) == 1){
            left_wheel.dutyCycle = 0;
            right_wheel.dutyCycle = 8000;
            Timer_A_generatePWM(TIMER_A0_BASE, &right_wheel);
@@ -132,7 +125,7 @@ void PORT5_IRQHandler(void)
     /*When Line Sensor (left) detects change from light to dark*/
     if (status & GPIO_PIN5){
         /*When Line Sensor detects dark*/
-        if(GPIO_getInputPinValue(GPIO_PORT_P5, GPIO_PIN5) == 0){
+        if(GPIO_getInputPinValue(GPIO_PORT_P5, GPIO_PIN5) == 1){
            left_wheel.dutyCycle = 8000;
            right_wheel.dutyCycle = 0;
            Timer_A_generatePWM(TIMER_A0_BASE, &right_wheel);
